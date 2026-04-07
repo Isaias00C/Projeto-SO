fa0275b701e4980b67f9959eec658c4efcb18a94
Isaias Castro <isaiascastro2014@gmail.com>
2026-04-07 18:03:11 -0300

    new: nova branch

--- src/MyThreads/ChildThread.java
+++ src/MyThreads/ChildThread.java
@@ -1,4 +1,65 @@
 package MyThreads;
 
-public class ChildThread {
+public class ChildThread extends Thread{
+    public final String name;
+    public Boolean comecou_com_bola = false;
+
+    public ChildThread(String name) {
+        super();
+        this.name = name;
+    }
+
+    public ChildThread(String name, Boolean comecou_com_bola) {
+        this.name = name;
+        this.comecou_com_bola = comecou_com_bola;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+            if (comecou_com_bola){
+                brinca();
+
+                descansa();
+
+                caminha_ate_cesto();
+
+                devolve_bola();
+
+                comecou_com_bola = false;
+            }else {
+                caminha_ate_cesto();
+
+                pega_bola();
+
+                volta_para_canto();
+
+                brinca();
+
+                descansa();
+
+                caminha_ate_cesto();
+
+                devolve_bola();
+            }
+        }
+    }
+
+    private void volta_para_canto() {
+    }
+
+    private void pega_bola() {
+    }
+
+    private void devolve_bola() {
+    }
+
+    private void descansa() {
+    }
+
+    private void caminha_ate_cesto() {
+    }
+
+    private void brinca() {
+    }
 }
